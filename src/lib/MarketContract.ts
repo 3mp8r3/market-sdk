25e33060329dce4fa05195256eefa7d358ab3db2
Apoorv <apoorvsingal0@gmail.com>
2022-03-01 22:21:36 +0530

    refactor: store web3 instance instead of provider

--- src/lib/MarketContract.ts
+++ src/lib/MarketContract.ts
@@ -1,15 +1,16 @@
 import Web3 from "web3";
-import { provider } from "web3-core";
 
 import { BaseContract } from "../types/types";
 
 class MarketContract<T extends BaseContract> {
   readonly address: string;
   readonly contract: T;
+  readonly web3: Web3;
 
-  constructor(provider: provider, address: string, abi: any[]){
+  constructor(web3: Web3, address: string, abi: any[]){
     this.address = address;
-    this.contract = <T><any>(new (new Web3(provider)).eth.Contract(abi, address));
+    this.web3 = web3;
+    this.contract = <T><any>(new web3.eth.Contract(abi, address));
   }
 }
 
