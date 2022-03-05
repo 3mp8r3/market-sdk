fc93007db79e63b23344d1cc94a20eeadbf9e2d0
Apoorv <apoorvsingal0@gmail.com>
2022-03-05 10:50:39 +0530

    chore: add serverless api endpoints

--- src/lib/MarketSDK.ts
+++ src/lib/MarketSDK.ts
@@ -9,6 +9,7 @@ import MarketAdmin from "./MarketAdmin";
 import CToken from "./CToken";
 
 class MarketSDK {
+  readonly apiURL: string = "https://sls.market.xyz/";
   readonly comptrollerAddress: string;
   readonly web3: Web3;
 
@@ -43,6 +44,34 @@ class MarketSDK {
   getCToken(address: string){
     this.checkInit();
     return new CToken(this.comptroller!, address);
+  }
+
+  async getTokenData(address: string, chainId?: number | string){
+    const _chainId = Number(chainId) || await this.web3.eth.getChainId();
+    const res = await this._getAPIReq(`tokenData?address=${address}&chainId=${_chainId}`);
+
+    return res;
+  }
+
+  getAllPools(format: boolean = false){
+    return this._getAPIReq(`getAllPools?format=${format ? 1 : 0}`);
+  }
+
+  getPoolsByOwner(owner: string, format: boolean = false){
+    return this._getAPIReq(`getPoolsByOwner?owner=${owner}&format=${format ? 1 : 0}`);
+  }
+
+  getPoolAssetsWithData(from: string){
+    return this._getAPIReq(`getPoolAssetsWithData?address=${from}&comptroller=${this.comptrollerAddress}`);
+  }
+
+  private async _getAPIReq(endpoint: string){
+    const res = await fetch(`${this.apiURL}/api/${endpoint}`)
+
+    if(res.status !== 200){
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+    return await res.json();
   }
 }
 
